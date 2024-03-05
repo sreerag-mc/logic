bd841554e9c8779b6b0998049d0be1ed6aba8f31
Sreerag Sree <sreeragappu421@gmail.com>
2024-03-05 13:36:10 +0530

    first commit

---
+++ patterns.py
@@ -0,0 +1,43 @@
+# n = 5
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(j + 65), end="")
+#     print()
+
+
+# for i in range(6, 1, -1):
+#     for j in range(1, i):
+#         print(j, end="")
+#     print()
+
+
+# n = int(input("Enter number of rows: "))
+#
+# for i in range(1,n+1):
+#     for j in range(1, i+1):
+#         print(j, end="")
+#     print()
+
+
+
+# def Check_Vow(string, vowels):
+#     final = [each for each in string if each in vowels]
+#     print(len(final))
+#     print(final)
+#
+# # Driver Code
+# string = "Geeks for Geeks"
+# vowels = "AaEeIiOoUu"
+# Check_Vow(string, vowels);
+
+# rows = 5
+# for i in range(rows):
+#     print(" " * (rows - i - 1), end="")
+#     print("*" * (2 * i + 1))
+
+
+# num_rows = 5
+# for i in range(num_rows, 0, -1):
+#     print(" " * (num_rows - i), end="")
+#     print(" " * (2 * i - 1))
+
